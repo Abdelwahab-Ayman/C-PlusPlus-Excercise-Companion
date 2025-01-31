55f71e667b884cfa1083a4f0127fc6041cc51d84
Abdelwahab Ayman Samy AbuGhaleb <57332184+Abdelwahab-Ayman@users.noreply.github.com>
2025-01-31 02:49:51 +0200

    Update 2-1.cpp

--- Chapter 2-Basics/Objects,Types and Vlaues/2-1.cpp
+++ Chapter 2-Basics/Objects,Types and Vlaues/2-1.cpp
@@ -1,1 +1,7 @@
+/*2.1 For each of the declarations below, do the following. State whether the declaration is also a definition. If the
+ declaration is not a definition, write a definition for the entity being declared. If the declaration is a definition,
+ write a declaration for the entity being declared that is not also a definition.*/
+int i = 5;
+// declaration and definition ,as it decaler an integar of data called 'i' and assign it to initial value which will equal 5
+int abs(int x);
 
